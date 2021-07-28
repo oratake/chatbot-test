ecdf8570e3c315ede0c84971000a2dcc93e50acb
oratake <smith.shimomura@gmail.com>
2021-07-28 23:27:09 +0900

    feat: チャット用のボックス表示

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import defaultDataset from './dataset';
 import './assets/styles/style.css';
 
-export class App extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -16,9 +16,9 @@ export class App extends React.Component {
 
   render() {
     return (
-      <>
-      </>
+      <section className="c-section">
+        <div className="c-box">{this.state.currentId}</div>
+      </section>
     );
   }
 }
-
