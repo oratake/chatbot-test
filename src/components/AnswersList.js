b1c843a832cf6f077f73bf795edf49ea971ca04b
oratake <smith.shimomura@gmail.com>
2021-07-28 23:51:14 +0900

    feat: Answer component テンプレ作成

--- src/components/AnswersList.js
+++ src/components/AnswersList.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Answer } from './index';
 
 const AnswersList = () => {
   return (
-    <></>
+    <div className="c-grid__answer">
+      <Answer />
+      <Answer />
+      <Answer />
+    </div>
   );
 };
 
