08367b40b96a6e88d577dcd588728902182ca18a
oratake <smith.shimomura@gmail.com>
2021-07-28 23:56:32 +0900

    feat: material-ui 導入

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+  },
+}));
 
 const Answer = () => {
+  // const classes = useStyles();
+
   return (
-    <></>
+    <Button variant="contained">Default</Button>
   );
 };
 
