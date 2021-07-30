4ce8a50ac6dc027647fdc4a8c63747d5f2116d28
oratake <smith.shimomura@gmail.com>
2021-07-31 01:54:48 +0900

    feat: 質問文をデータから表示するように

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import { ListItem, ListItemAvatar, Avatar } from '@material-ui/core';
 
-const Chat = () => {
+type Props = {
+  type: String,
+  text: String,
+};
+
+const Chat = (props: Props) => {
+  const isQuestion = (props.type === 'question');
+  const classes = isQuestion ? 'p-chat__row' : 'p-chat__reverse';
+
   return (
-    <ListItem alignItems="flex-start">
+    <ListItem className={classes}>
       <ListItemAvatar>
         <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
       </ListItemAvatar>
-      <div className="p-chat__bubble">TEXT</div>
+      <div className="p-chat__bubble">{props.text}</div>
     </ListItem>
   );
 };
