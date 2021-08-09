0705dc74366d3e17f1ff8680abe2943efc1f8b45
oratake <smith.shimomura@gmail.com>
2021-08-09 20:07:26 +0900

    refactor: Answer to tsx

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
+type Props = {
+  content: String,
+  nextId: String,
+  key: String,
+  select: () => void, 
+};
+
 const useStyles = makeStyles((theme) => ({
   root: {
   },
 }));
 
-const Answer = (props) => {
+const Answer = (props: Props) => {
   // const classes = useStyles();
 
   return (
-    <Button variant="contained" color="primary">
+    <Button variant="contained" color="primary" onClick={() => props.select(props.content, props.nextId)}>
       {props.content}
     </Button>
   );
