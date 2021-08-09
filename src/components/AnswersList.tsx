938ba6a33f661bd12d3f02a859641d1f0033c3ff
oratake <smith.shimomura@gmail.com>
2021-08-09 20:11:08 +0900

    feat: chatにnextIdとして次の質問IDを渡す

--- src/components/AnswersList.tsx
+++ src/components/AnswersList.tsx
@@ -13,7 +13,7 @@ const AnswersList = (props: Props) => {
   return (
     <div className="c-grid__answer">
       {props.answers.map((value, index) => {
-        return <Answer content={value.content} key={index.toString()} select={props.select} />
+        return <Answer content={value.content} nextId={value.nextId} key={index.toString()} select={props.select} />
       })}
     </div>
   );
