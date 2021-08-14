3b4b49987825e363b9d32d0a507506541eed6dc4
oratake <smith.shimomura@gmail.com>
2021-08-14 10:45:29 +0900

    fix: formDialogコンポーネントをindexに追加

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as AnswersList } from './AnswersList.tsx';
 export { default as Answer } from './Answer.tsx';
 export { default as Chats } from './Chats.tsx';
 export { default as Chat } from './Chat.tsx';
+export { default as FormDialog } from './Forms/FormDialog.tsx';
